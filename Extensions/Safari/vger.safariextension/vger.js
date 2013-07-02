d6329e503747c51aafffd0d7c2f1265f3dc3a732
chendesheng <ichendesheng@gmail.com>
2013-07-03 00:25:45 +0800

    Sample and powerful safari plugin.

---
+++ Extensions/Safari/vger.safariextension/vger.js
@@ -0,0 +1,32 @@
+document.addEventListener("contextmenu", handleContextMenu, false);
+
+function handleContextMenu(event) {
+	// safari.self.tab.setContextMenuEventUserInfo(event, event.target);
+	if (event.target.tagName == 'A') {
+		safari.self.tab.dispatchMessage("contextmenu", event.target.href);
+	} else {
+		var currentElement = event.target;
+		while (currentElement != null) {
+			if (currentElement.tagName == 'A') {
+				break;
+			}
+			currentElement = currentElement.parentNode;
+		}
+		if (currentElement.tagName == 'A') {
+			safari.self.tab.dispatchMessage("contextmenu", currentElement.href);
+		} else {
+			safari.self.tab.dispatchMessage("contextmenu", "");
+		}
+	}
+
+	// safari.self.tab.dispatchMessage('sendNsaArray');
+}
+
+function handleMessage(event) {
+	if (event.name == 'alert') {
+		alert(event.message);
+	}
+}
+
+safari.self.addEventListener("message", handleMessage, false);
+
