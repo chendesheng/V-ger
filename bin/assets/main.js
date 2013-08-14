17dd78b6314727c00f5d3c9dfcea5b88da98d31a
chendesheng <ichendesheng@gmail.com>
2013-08-15 02:03:37 +0800

    Change to angularjs1.2rc1, add animation to task list. Pretty cool.

--- bin/assets/main.js
+++ bin/assets/main.js
@@ -1,34 +1,36 @@
-angular.module('vger', ['ui']).controller('tasks_ctrl',
-	function ($scope, $http) {
+angular.module('vger', ['ngAnimate']).controller('tasks_ctrl',
+	function($scope, $http) {
 
 		function monitor(path, ondata, onclose, onerror) {
-			var websocket = new WebSocket('ws://' + 
+			var websocket = new WebSocket('ws://' +
 				window.location.host + path);
 
-			websocket.onopen = function(evt) { onOpen(evt) };
-			websocket.onclose = function(evt) { onClose(evt) };
-			websocket.onmessage = function(evt) { onMessage(evt) };
-			websocket.onerror = function(evt) { onError(evt) };
+			websocket.onopen = onOpen;
+			websocket.onclose = onClose;
+			websocket.onmessage = onMessage;
+			websocket.onerror = onError;
 
-			function onOpen(evt) { 
-			}
-			function onClose(evt) { 
+			function onOpen(evt) {}
+
+			function onClose(evt) {
 				// $scope.push_alert('socket close');
 				if (onclose) {
 					onclose(evt);
 				}
 			}
-			function onMessage(evt) { 
+
+			function onMessage(evt) {
 				ondata(JSON.parse(evt.data));
-			}  
-			function onError(evt) { 
+			}
+
+			function onError(evt) {
 				// $scope.push_alert(evt.data);
 				if (onerror) {
 					onerror(evt.data);
 				}
 			}
 
-			function doSend(message) { 
+			function doSend(message) {
 				websocket.send(message);
 			}
 		}
@@ -44,17 +46,19 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 				};
 
 				$scope.$apply(function() {
-					var collection  =  {};
+					var collection = {};
 					for (var i = data.length - 1; i >= 0; i--) {
 						var item = data[i]
-						collection[item.Name] = item;
+						if (item.Status != "Deleted") {
+							collection[item.Name] = item;
+						}
 					};
 					var tasks = $scope.tasks;
 					for (var i = tasks.length - 1; i >= 0; i--) {
 						var task = tasks[i];
 						var source = collection[task.Name];
 						if (source) {
-							angular.forEach(source, function (val, key) {
+							angular.forEach(source, function(val, key) {
 								task[key] = val;
 							});
 							delete collection[task.Name];
@@ -62,72 +66,66 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 							tasks.splice(i, 1);
 						}
 					}
-					angular.forEach(collection, function (val, key) {
+					angular.forEach(collection, function(val, key) {
 						tasks.push(val)
 					});
 				});
 			}, monitor_process);
 		}
 		monitor_process();
-		// function init() {
-		// 	$http.get('/progress').success(function(data) {
-		// 		for (var i = data.length - 1; i >= 0; i--) {
-		// 			var item = data[i];
-		// 			item.StartDate = new Date(Date.parse(item.StartDate))
-		// 		};
-		// 		$scope.tasks = data;
-		// 	})
-		// }
 
-		// init();
-		
 
 		$scope.parse_duration = function(dur) {
-			var sec = Math.floor(dur/1000000000);
-			var min = Math.floor(sec/60);
-			var hour = Math.floor(min/60);
-			var day = Math.floor(hour/24);
+			var sec = Math.floor(dur / 1000000000);
+			var min = Math.floor(sec / 60);
+			var hour = Math.floor(min / 60);
+			var day = Math.floor(hour / 24);
 
-			return (day>0?day+'d':'')+(hour>0?hour%24+'h':'')+(min>0?min%60+'m':'')+(sec>0?sec%60+'s':'');
+			return (day > 0 ? day + 'd' : '') + (hour > 0 ? hour % 24 + 'h' : '') + (min > 0 ? min % 60 + 'm' : '') + (sec > 0 ? sec % 60 + 's' : '');
 		}
 
-		$scope.send_open = function (task) {
-			$http.get('/open/' + task.Name).success(function (resp) {
+		$scope.send_open = function(task) {
+			$http.get('/open/' + task.Name).success(function(resp) {
 				resp && $scope.push_alert(resp);
 			});
 		}
-		$scope.send_resume = function (task) {
-			$http.get('/resume/' + task.Name).success(function (resp) {
+		$scope.send_resume = function(task) {
+			$http.get('/resume/' + task.Name).success(function(resp) {
 				resp && $scope.push_alert(resp);
 			});
 		}
-		$scope.send_stop = function (task) {
-			$http.get('/stop/' + task.Name).success(function (resp) {
+		$scope.send_stop = function(task) {
+			$http.get('/stop/' + task.Name).success(function(resp) {
 				resp && $scope.push_alert(resp);
 			});
 		}
-		$scope.send_limit = function (task) {
-			$http.post('/limit/' + task.Name, task.LimitSpeed).success(function (resp) {
+		$scope.send_limit = function(task) {
+			$http.post('/limit/' + task.Name, task.LimitSpeed).success(function(resp) {
 				resp && $scope.push_alert(resp);
 			});
 		};
-		$scope.send_play = function (task) {
-			$http.get('/play/' + task.Name).success(function (resp) {
+		$scope.send_play = function(task) {
+			$http.get('/play/' + task.Name).success(function(resp) {
 				resp && $scope.push_alert(resp);
 			})
 		};
 
 		$scope.waiting = false;
+
 		function new_task() {
 			$scope.waiting = true;
 			if ($scope.new_url.indexOf('lixian.vip.xunlei.com') != -1 ||
-				$scope.new_url.indexOf('youtube.com') != -1 || 
-				/.*dmg$/.test($scope.new_url)) {
+				$scope.new_url.indexOf('youtube.com') != -1 ||
+				/.*dmg|.*zip|.*rar$/.test($scope.new_url)) {
 				$http.post('/new/', $scope.new_url).success(function(resp) {
-					$scope.new_url = '';
+					if (!resp) {
+						// $scope.new_url = '';
+					}
 					$scope.waiting = false;
 					resp && $scope.push_alert(resp);
-				}).error(function(){$scope.waiting = false;});
+				}).error(function() {
+					$scope.waiting = false;
+				});
 			} else {
 				$http.post('/thunder/new', $scope.new_url).success(function(data) {
 					$scope.waiting = false;
@@ -138,6 +136,9 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 					for (var i = data.length - 1; i >= 0; i--) {
 						var item = data[i];
 						item.loading = false;
+
+						var j = item.name.lastIndexOf('\/');
+						item.name = item.name.substring(j + 1);
 					}
 					if (data.length == 1 && data[0].Percent == 100) {
 						$scope.waiting = true;
@@ -156,32 +157,31 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 			file.loading = true;
 
 			$http.post('/new/' + file.Name, file.DownloadURL).success(
-			function(resp) {
-				file.loading = false;
-				$scope.waiting = false;
-				if (resp) $scope.push_alert(resp);
-				else {
-					// $scope.bt_files = [];
-					$scope.new_url = '';
-				}
-			}).error(function() {
+				function(resp) {
+					file.loading = false;
+					$scope.waiting = false;
+					if (resp) $scope.push_alert(resp);
+					else {
+						// $scope.bt_files = [];
+						$scope.new_url = '';
+					}
+				}).error(function() {
 				file.loading = false;
 				$scope.waiting = false;
 			});
 		};
 		$scope.bt_files = [];
 
-		$scope.move_to_trash = function (task) {
+		$scope.move_to_trash = function(task) {
 			$http.get('/trash/' + task.Name).success(
-			function (resp) {
-				resp && $scope.push_alert(resp)
-			});
+				function(resp) {
+					resp && $scope.push_alert(resp)
+				});
 		};
 
-		$scope.set_autoshutdown = function (task) {
-			$http.post('/autoshutdown/' + task.Name, task.Autoshutdown?'on':'off')
-				.success(function(){
-				});
+		$scope.set_autoshutdown = function(task) {
+			$http.post('/autoshutdown/' + task.Name, task.Autoshutdown ? 'on' : 'off')
+				.success(function() {});
 		};
 
 
@@ -191,7 +191,7 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 
 		$scope.search_subtitles = function(name) {
 			$scope.subtitles_movie_name = name;
-			$http.get('/subtitles/search/' + name).success(function (data) {
+			$http.get('/subtitles/search/' + name).success(function(data) {
 				if (data.length == 0) {
 					$scope.nosubtitles = true;
 					$scope.waiting = false;
@@ -206,7 +206,7 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 					item.FullDescription = item.Description;
 					var description = item.Description;
 					if (description.length > 73)
-						item.Description = description.substr(0, 35) + '...' + description.substr(description.length-35, 35);
+						item.Description = description.substr(0, 35) + '...' + description.substr(description.length - 35, 35);
 
 				};
 				$scope.subtitles = data;
@@ -214,15 +214,15 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 			});
 		};
 
-		$scope.download_subtitles = function (sub) {
+		$scope.download_subtitles = function(sub) {
 			sub.loading = true;
-			$http.post('/subtitles/download/'+$scope.subtitles_movie_name, sub.URL).success(function () {
+			$http.post('/subtitles/download/' + $scope.subtitles_movie_name, sub.URL).success(function() {
 				sub.loading = false;
 				$scope.subtitles = [];
 			})
 		};
 
-		$scope.go = function () {
+		$scope.go = function() {
 			$scope.waiting = true
 			if (/.+\:\/\/.+|^magnet\:\?.+/.test($scope.new_url)) {
 				new_task();
@@ -236,7 +236,7 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 			window.open("http://www.google.com/search?q=" + encodeURIComponent(name));
 			$scope.nosubtitles = false;
 		};
-		$scope.addic7ed_subtitles = function () {
+		$scope.addic7ed_subtitles = function() {
 			var name = $scope.subtitles_movie_name;
 			var i = name.lastIndexOf('.');
 			if (i != -1) {
@@ -253,21 +253,21 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 			name = name.replace(/[.]/g, ' ');
 
 			window.open("http://www.addic7ed.com/search.php?search=" + encodeURIComponent(name));
-			$scope.nosubtitles = false;	
+			$scope.nosubtitles = false;
 		}
 
 
-		$scope.parse_time = function (time) {
+		$scope.parse_time = function(time) {
 			var d = new Date(time * 1000);
 			return d.format('ddd mmm dd')
 		}
-		$scope.upload_torrent = function ($event) {
+		$scope.upload_torrent = function($event) {
 			$event.preventDefault();
-			
+
 			if ($event.dataTransfer.files.length == 0) {
 				var items = $event.dataTransfer.items;
-				if (items && items.length > 0 && items[0].type=='text/plain') {
-					items[0].getAsString(function(str){
+				if (items && items.length > 0 && items[0].type == 'text/plain') {
+					items[0].getAsString(function(str) {
 						$scope.$apply(function() {
 							$scope.new_url = str;
 						})
@@ -290,22 +290,22 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 			xhr.open('POST', '/thunder/torrent');
 			xhr.send(fd);
 			xhr.onreadystatechange = function() {
-				 if(this.readyState == this.DONE) {
-				 	// if (!$scope.waiting)) return;
-					$scope.$apply(function () {
-					 	$scope.waiting = false;
+				if (this.readyState == this.DONE) {
+					// if (!$scope.waiting)) return;
+					$scope.$apply(function() {
+						$scope.waiting = false;
 					});
 
-				    if(this.status == 200 && this.responseText != null) {
-				    	var responseText = this.responseText;
-				    	$scope.$apply(function () {
-				    		if (responseText[0] != '[') {
-				    			responseText && $scope.push_alert(responseText);
-					    	} else {
-						    	$scope.bt_files = JSON.parse(responseText);
-						    }
-				        });
-				    }
+					if (this.status == 200 && this.responseText != null) {
+						var responseText = this.responseText;
+						$scope.$apply(function() {
+							if (responseText[0] != '[') {
+								responseText && $scope.push_alert(responseText);
+							} else {
+								$scope.bt_files = JSON.parse(responseText);
+							}
+						});
+					}
 				}
 			}
 		}
@@ -313,7 +313,10 @@ angular.module('vger', ['ui']).controller('tasks_ctrl',
 		$scope.alerts = [];
 		$scope.push_alert = function(content, title) {
 			title = title || 'Error';
-			$scope.alerts.push({'title': title, 'content': content});
+			$scope.alerts.push({
+				'title': title,
+				'content': content
+			});
 		}
 		$scope.pop_alert = function() {
 			$scope.alerts.pop();
