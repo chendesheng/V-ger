085ec17d30c6cb96dfce622789895472c6b2846b
chendesheng <ichendesheng@gmail.com>
2013-08-17 01:10:20 +0800

    Fix play online bug

--- src/download/play.go
+++ src/download/play.go
@@ -14,7 +14,7 @@ import (
 var play_quit chan bool
 
 func Play(t *task.Task, w io.Writer, from, to int64) {
-	fmt.Println("from ", from, " to ", to)
+	fmt.Println("playing download from ", from, " to ", to)
 	if play_quit != nil {
 		ensureQuit(play_quit)
 	}
@@ -27,5 +27,4 @@ func Play(t *task.Task, w io.Writer, from, to int64) {
 	control := make(chan int)
 	progress := doDownload(t.URL, w, from, to, 0, control, play_quit)
 	handleProgress(progress, t, play_quit)
-	go ensureQuit(play_quit)
 }
