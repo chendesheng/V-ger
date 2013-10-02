05aaed59c1433712a6d26f641e0032d90d8184a7
chendesheng <ichendesheng@gmail.com>
2013-10-02 20:22:34 +0800

    Remove some files.

---
+++ src/native/native.go
@@ -0,0 +1,39 @@
+package native
+
+type NotificationClickCallback func(string, string)
+
+type NativeAPI interface {
+	SendNotification(title, infoText string)
+	SetNotificationClickCallback(callbock NotificationClickCallback)
+	ComputerShutdown(reason string) error
+	MoveFileToTrash(dir, name string) error
+}
+
+var DefaultNativeAPI NativeAPI
+
+func SendNotification(title, infoText string) {
+	if DefaultNativeAPI != nil {
+		DefaultNativeAPI.SendNotification(title, infoText)
+	}
+}
+
+func Shutdown(reason string) error {
+	if DefaultNativeAPI != nil {
+		return DefaultNativeAPI.ComputerShutdown(reason)
+	}
+	return nil
+}
+
+func MoveFileToTrash(dir, name string) error {
+	if DefaultNativeAPI != nil {
+		return DefaultNativeAPI.MoveFileToTrash(dir, name)
+	}
+
+	return nil
+}
+
+func SetNotificationClickCallback(callback NotificationClickCallback) {
+	if DefaultNativeAPI != nil {
+		DefaultNativeAPI.SetNotificationClickCallback(callback)
+	}
+}
