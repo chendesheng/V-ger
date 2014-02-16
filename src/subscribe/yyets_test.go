29dd2cca1ecf58397f95077a37da70424ddb52e2
chendesheng <ichendesheng@gmail.com>
2014-02-16 23:57:06 +0800

    Add another test

--- src/subscribe/yyets_test.go
+++ src/subscribe/yyets_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestYYest(t *testing.T) {
+func TestYYets(t *testing.T) {
 	f, err := os.Open("yyets.html")
 	if err != nil {
 		t.Error(err)
@@ -24,4 +24,25 @@ func TestYYest(t *testing.T) {
 	fmt.Printf("%v\n", tasks[0])
 	fmt.Printf("%v\n", tasks[1])
 	fmt.Printf("%v\n", tasks[2])
+}
+
+func TestYYets2(t *testing.T) {
+	f, err := os.Open("b.html")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_, tasks, err := parse(f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if len(tasks) != 4 {
+		t.Errorf("Expect 4 tasks but %d", len(tasks))
+	}
+
+	fmt.Printf("%v\n", tasks[0])
+	fmt.Printf("%v\n", tasks[1])
+	fmt.Printf("%v\n", tasks[2])
+	fmt.Printf("%v\n", tasks[3])
 }
