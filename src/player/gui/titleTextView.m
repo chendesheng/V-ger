5a2fffbfb46805c7e216cb777d27cac06e5eeabc
chendesheng <ichendesheng@gmail.com>
2014-06-28 20:21:24 +0800

    Center window title; truncate in middle.

--- src/player/gui/titleTextView.m
+++ src/player/gui/titleTextView.m
@@ -12,10 +12,15 @@
 
 - (void)drawRect:(NSRect)dirtyRect {
     [[NSColor blackColor] setFill];
-    NSDictionary *attr = @{NSFontAttributeName : [NSFont titleBarFontOfSize:13]};
-    NSSize titlesz = [self->_title sizeWithAttributes:attr];
-    [self->_title drawAtPoint:NSMakePoint((dirtyRect.size.width-titlesz.width)/2, 3) withAttributes:attr];
+    
+    NSMutableParagraphStyle *style = [[NSParagraphStyle defaultParagraphStyle] mutableCopy];
+	[style setAlignment:NSCenterTextAlignment];
+	[style setLineBreakMode:NSLineBreakByTruncatingMiddle];
 
+    NSDictionary *attr = @{NSFontAttributeName : [NSFont titleBarFontOfSize:13], NSParagraphStyleAttributeName: style};
+    // NSSize titlesz = [self->_title sizeWithAttributes:attr];
+    // [self->_title drawAtPoint:NSMakePoint((dirtyRect.size.width-titlesz.width)/2, 3) withAttributes:attr];
+    [self->_title drawWithRect:NSMakeRect(82, 6, self.bounds.size.width-164, 16) options:NSStringDrawingTruncatesLastVisibleLine attributes:attr];
     [super drawRect:dirtyRect];
 }
 @end
