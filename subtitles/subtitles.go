1bd466f5fd36febeda728d1a87ff9c71724c1f8e
chendesheng <ichendesheng@gmail.com>
2014-11-27 22:27:31 +0800

    Remove shooter subtitle search

--- subtitles/subtitles.go
+++ subtitles/subtitles.go
@@ -28,7 +28,6 @@ func (s *Subtitle) String() string {
 func SearchSubtitles(name string, url string, result chan Subtitle, quit chan struct{}) {
 	searchers := []subSearcher{
 		&yyetsSearch{name, 1, quit},
-		&shooterSearch{name, 2, quit},
 		&kankanSearch{url, quit},
 		&addic7ed{name, quit},
 	}
